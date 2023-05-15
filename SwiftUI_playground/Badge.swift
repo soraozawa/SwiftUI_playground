6a117d442bc698c46501fedfea9726c8266dff14
Sora Ozawa <soraozawa56@gmail.com>
2023-05-16 07:43:58 +0900

    Badge.swift, ProfileSummary.swift, HikeBadge.swift

---
+++ SwiftUI_playground/Badge.swift
@@ -0,0 +1,20 @@
+//
+//  Badge.swift
+//  SwiftUI_playground
+//
+//  Created by 小澤空 on 2023/05/16.
+//
+
+import SwiftUI
+
+struct Badge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Badge_Previews: PreviewProvider {
+    static var previews: some View {
+        Badge()
+    }
+}
