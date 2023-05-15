625c467a9a24ca5f52e22e9efac0df79922e9266
Sora Ozawa <soraozawa56@gmail.com>
2023-05-16 07:08:18 +0900

    Initial Commit

---
+++ SwiftUI_playground/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftUI_playground
+//
+//  Created by 小澤空 on 2023/05/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_playground/SwiftUI_playgroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_playgroundApp.swift
+//  SwiftUI_playground
+//
+//  Created by 小澤空 on 2023/05/16.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_playgroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
